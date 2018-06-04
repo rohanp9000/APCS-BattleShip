a520eb2a58d7ae31dd907d05b14493029c025d2e
rohanp9000 <rohanp9000@gmail.com>
2018-06-04 08:29:55 -0400

    Updated board after changes on 6/4

--- Board.java
+++ Board.java
@@ -1,114 +1,74 @@
-package UI;
+import BreezySwing.*;
+
+import java.awt.Color;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
 import javax.swing.*;
 import javax.swing.border.*;
 
+import javax.swing.*;
 
-
-public class Board implements ActionListener{
-
-    private final JPanel gui = new JPanel(new BorderLayout(3, 3));
-    private JButton[][] chessBoardSquares = new JButton[12][12];
-    private JPanel chessBoard;
-    private final JLabel message = new JLabel("");
-
-    Board() {
-        initializeGui();
-    }
-
-    public final void initializeGui() {
-        // set up the main GUI
-        gui.setBorder(new EmptyBorder(5, 5, 5, 5));
-        JToolBar tools = new JToolBar();
-        tools.setFloatable(false);
-        gui.add(tools, BorderLayout.PAGE_START);
-     
-        tools.add(message);
-
-      
-
-        chessBoard = new JPanel(new GridLayout(0,11));
-        chessBoard.setBorder(new LineBorder(Color.BLUE));
-        gui.add(chessBoard);
-
-        // create the chess board squares
-        Insets buttonMargin = new Insets(0,0,0,0);
-        for (int ii = 0; ii < chessBoardSquares.length; ii++) {
-            for (int jj = 0; jj < chessBoardSquares[ii].length; jj++) {
-                JButton b = new JButton();
-                b.setMargin(buttonMargin);
-                // our chess pieces are 64x64 px in size, so we'll
-                // 'fill this in' using a transparent icon..
-                ImageIcon icon = new ImageIcon(
-                        new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
-                b.setIcon(icon);
+public class Board extends GBFrame{
+	JButton [][] arrayButtons = new JButton[11][11];
+	
+	public Board(){
+		for(int i =0; i < arrayButtons.length; i++){
+			for(int j = 1; j < arrayButtons[0].length; j++){
+				JButton b = new JButton();
+				
                 
-                b.setBackground(Color.WHITE);
-               
-                chessBoardSquares[jj][ii] = b;
-            }
-        }
-
-        //fill the chess board
-        chessBoard.add(new JLabel(""));
-        // fill the top row
-        for (int ii = 0; ii < 10; ii++) {
-            chessBoard.add(new JLabel(Integer.toString(ii+1),SwingConstants.CENTER));
-        }
-        // fill the black non-pawn piece row
-        for (int ii = 1; ii < 11; ii++) {
-            for (int jj = 0; jj < 11; jj++) {
-            			if(jj ==0){
-            				chessBoard.add(new JLabel(Integer.toString(ii),SwingConstants.CENTER));
-            			}
-            			else{
-            				chessBoard.add(chessBoardSquares[jj][ii]);
-            			}
-            }
-        }
-    }
-    public void buttonClicked(JButton buttonObj){
-  
-    }
-    public final JComponent getChessBoard() {
-        return chessBoard;
-    }
-
-    public final JComponent getGui() {
-        return gui;
-    }
-
-    public static void main(String[] args) {
-        Runnable r = new Runnable() {
-
-            @Override
-            public void run() {
-                Board cb =
-                        new Board();
-
-                JFrame f = new JFrame("ChessChamp");
-                f.add(cb.getGui());
-                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                f.setLocationByPlatform(true);
-
-                // ensures the frame is the minimum size it needs to be
-                // in order display the components within it
-                f.pack();
-                // ensures the minimum size is enforced.
-                f.setMinimumSize(f.getSize());
-                f.setVisible(true);
-            }
-        };
-        SwingUtilities.invokeLater(r);
-    }
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
+				b =  addButton("", i, j, 1, 1);
+				
+                b.setIcon(new ImageIcon("src/img4.jpg"));
+                
+                b.setBackground(Color.BLUE);
+                arrayButtons[i][j] = b;
+			}
+		}
 	}
+
+	public void buttonClicked(JButton buttonObj){
+		for(int i =0; i < arrayButtons.length; i++){
+			for(int j = 0; j < arrayButtons[0].length; j++){
+				if(buttonObj == arrayButtons[i][j]){
+					
+					if(Math.random()<.5){
+						arrayButtons[i][j].setIcon(new ImageIcon("src/img2.jpg"));
+						arrayButtons[i][j].setBackground(Color.RED);
+					}
+					else{
+					
+		                ImageIcon icon = new ImageIcon(new BufferedImage(299, 168, BufferedImage.TYPE_INT_ARGB));
+		                arrayButtons[i][j].setIcon(icon);
+		                arrayButtons[i][j].setBackground(Color.WHITE);
+					}	
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Main Method
+	 *
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		Board GUI = new Board();
+		GUI.setSize(700, 700);
+		GUI.setVisible(true);
+		
+		Board GUI2 = new Board();
+		GUI2.setSize(700, 700);
+		GUI2.setVisible(true);
+
+
+	}
+
 }
+
+
+
+
 
